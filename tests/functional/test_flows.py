7c047724523dfe41e3444e361dc4e790df509b42
winkidney <winkidney@gmail.com>
2024-10-27 18:58:50 +0800

    feature: add more test details for coin price fetch

--- tests/functional/test_flows.py
+++ tests/functional/test_flows.py
@@ -1,6 +1,13 @@
+import time
+
 from etl_homework import flows
+from etl_homework import models
 
 
 def test_coin_price_fetch(prefect_unittest_conf):
     ret = flows.task_fetch_current_coin_price("BTC", "USD")
     assert ret.id is not None
+    cp = models.CoinPrice.select().where(models.CoinPrice.id == ret.id).get()
+    assert cp is not None
+    assert time.time() - cp.start_timestamp.timestamp() < 1, cp.start_timestamp
+    assert cp.price > 0, cp.price
