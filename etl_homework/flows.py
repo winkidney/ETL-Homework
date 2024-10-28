3aac0100292a1d169939f6ace8f7063063a95243
winkidney <winkidney@gmail.com>
2024-10-28 13:03:33 +0800

    refactor: log price getting info

--- etl_homework/flows.py
+++ etl_homework/flows.py
@@ -19,18 +19,21 @@ def hello_world(name: str = "world", goodbye: bool = False):
 
 @task(name="get-btc-price", task_run_name="get-btc-price-in-{coin}/{currency}")
 def task_fetch_current_coin_price(coin: str, currency: str):
-    client = BinanceAPICrawler(get_run_logger())
+    logger = get_run_logger()
+    client = BinanceAPICrawler(logger)
     recent_price = client.get_historic_price(
         coin=coin, currency=currency, timeframe=TimeFrames.FIVE_MINUTES
     )[-1]
-    price_column = models.CoinPrice(
+    price_column, created = models.CoinPrice.get_or_create(
         coin=coin,
         currency=currency,
         start_timestamp=recent_price[0],
         price=recent_price[1],
         timeframe=TimeFrames.FIVE_MINUTES,
     )
-    price_column.save()
+    logger.info(
+        f"\nPrice updated: pair={coin}/{currency} ts={price_column.start_timestamp}, created={created}, value={price_column.price}"
+    )
     return price_column
 
 
@@ -44,23 +47,25 @@ def task_fetch_current_btc_network_hash_rate_and_difficulty():
     hash_rate, difficulty = client.get_current_hash_rate_difficulty()
     recent_timestamp = get_last_recent_n_minutes_timestamp(int(time.time()), minutes=5)
     with dbs.crawler_db_proxy.atomic():
-        nd = models.NetworkDifficulty(
+        models.NetworkDifficulty.select("id").where(
+            models.NetworkDifficulty.start_timestamp == recent_timestamp,
+            models.NetworkDifficulty.timeframe == TimeFrames.FIVE_MINUTES,
+        )
+        nd, nd_created = models.NetworkDifficulty.get_or_create(
             network="BTC",
             start_timestamp=recent_timestamp,
             timeframe=TimeFrames.FIVE_MINUTES,
             difficulty=difficulty,
         )
-        nhr = models.NetworkHashRate(
+        nhr, nhr_created = models.NetworkHashRate.get_or_create(
             network="BTC",
             start_timestamp=recent_timestamp,
             timeframe=TimeFrames.FIVE_MINUTES,
             hash_rate=hash_rate,
         )
-        nd.save()
-        nhr.save()
     logger.info(
-        f"\nHashRate:   ts={nhr.start_timestamp}, value={nhr.hash_rate}"
-        + f"\nDifficulty: ts={nd.start_timestamp}, value={nd.difficulty}"
+        f"\nHashRate:  ts={nhr.start_timestamp}, created={nd_created}, value={nhr.hash_rate}"
+        + f"\nDifficulty: ts={nd.start_timestamp}, created={nhr_created}, value={nd.difficulty}"
     )
     return nd, nhr
 
