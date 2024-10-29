386b8aee637ad9fd0d60015bae28ec533eab8b73
winkidney <winkidney@gmail.com>
2024-10-29 17:06:49 +0800

    feature: the interpolate_data function works well + with basic performance test

--- etl_homework/transformers.py
+++ etl_homework/transformers.py
@@ -1,6 +1,13 @@
+import numpy as np
+import pandas as pd
+
 from etl_homework import models
 from etl_homework.crawler import TimeFrames
 from etl_homework.utils.time import get_last_recent_n_minutes_timestamp
+
+
+class DataQualityTooLow(ValueError):
+    pass
 
 
 def get_ts_range_by_timeframe(
@@ -44,8 +51,93 @@ def generate_tf_indexes(start_timestamp: int, end_timestamp: int, time_frame: st
     return indexes
 
 
-def interpolate_data(ts_indexes, original_data):
-    pass
+def interpolate_data_for_small_numbers(
+    ts_indexes,
+    original_ts_list: list or tuple,
+    original_data: list or tuple,
+    out_type=float,
+):
+    columns = ("2be_interpolate",)
+    data_set = {
+        "2be_interpolate": original_data,
+    }
+    series = pd.DataFrame(
+        data=data_set,
+        index=original_ts_list,
+        columns=columns,
+    )
+    series["2be_interpolate"] = pd.to_numeric(series["2be_interpolate"])
+    series = series.reindex(index=ts_indexes)
+    if str(series.iloc[0, 0]) == str(np.nan):
+        raise DataQualityTooLow("quality low, starting point missed")
+    if str(series.iloc[-1, 0]) == str(np.nan):
+        raise DataQualityTooLow("quality low, end point missed")
+    interpolated = series.interpolate(method="linear", columns=columns)
+    out_data = tuple(interpolated["2be_interpolate"].astype(out_type))
+    return out_data
+
+
+def interpolate_data(
+    ts_indexes,
+    original_ts_list: list or tuple,
+    original_data: list or tuple,
+    force_int=False,
+):
+    """
+    Perform linear interpolation on given time series data.
+    interpolate method in Numpy and pandas doesn't support python's super-big int type.
+    hash-rate number is out of range of Int64 or Float64.
+
+    :param ts_indexes: The list of time indices for the full range.
+    :param original_ts_list: The original timestamps corresponding to the original data.
+    :param original_data: The list of original data points, with None for missing values.
+    :param force_int: force the type to handle super-big int, required for super-big int.
+    :return: tuple: A tuple of interpolated values in the specified output type.
+    """
+    # TODO(winkidney): may have performance issue on super-large arrays.
+    ts_indexes = ts_indexes
+    original_ts_list = original_ts_list
+    original_data = original_data
+
+    interpolated_data = [None] * len(ts_indexes)
+
+    for original_ts, data in zip(original_ts_list, original_data):
+        if data is not None:
+            index = ts_indexes.index(original_ts)
+            interpolated_data[index] = data
+
+    for idx2fill in range(len(interpolated_data)):
+        if interpolated_data[idx2fill] is None:
+            # Find the nearest known points on the left and right
+            left_index = None
+            right_index = None
+
+            for index in range(idx2fill - 1, -1, -1):
+                if interpolated_data[index] is not None:
+                    left_index = index
+                    break
+
+            for index in range(idx2fill + 1, len(interpolated_data)):
+                if interpolated_data[index] is not None:
+                    right_index = index
+                    break
+
+            if left_index is not None and right_index is not None:
+                x0 = ts_indexes[left_index]
+                y0 = interpolated_data[left_index]
+                x1 = ts_indexes[right_index]
+                y1 = interpolated_data[right_index]
+
+                if force_int:
+                    interpolated_data[idx2fill] = y0 + (y1 - y0) * (
+                        ts_indexes[idx2fill] - x0
+                    ) // (x1 - x0)
+                else:
+                    interpolated_data[idx2fill] = y0 + (y1 - y0) * (
+                        ts_indexes[idx2fill] - x0
+                    ) / (x1 - x0)
+
+    return tuple(interpolated_data)
 
 
 class CoinMetrics:
--- tests/unittest/test_transformers.py
+++ tests/unittest/test_transformers.py
@@ -1,3 +1,5 @@
+import timeit
+
 import pytest
 
 from etl_homework import transformers
@@ -57,3 +59,96 @@ def test_should_get_correct_tf_indexes(
     if expected_num_ts > 0:
         assert indexes[0] == start_ts, indexes
         assert indexes[-1] == end_ts, indexes
+
+
+@pytest.mark.parametrize(
+    "is_super_big_int, ts_indexes, original_ts, original_data, expected",
+    (
+        (
+            False,
+            (0, 300, 600, 900, 1200),
+            (0, 600, 900, 1200),
+            (0, 60, 90, 120),
+            (0, 30, 60, 90, 120),
+        ),
+        (
+            False,
+            (0, 300, 600, 900, 1200),
+            (0, 600, 1200),
+            (0, 60, 120),
+            (0, 30, 60, 90, 120),
+        ),
+        (
+            True,
+            (0, 300, 600, 900, 1200),
+            (0, 600, 900, 1200),
+            (0, 200000000000000000000, 300000000000000000000, 400000000000000000000),
+            (
+                0,
+                100000000000000000000,
+                200000000000000000000,
+                300000000000000000000,
+                400000000000000000000,
+            ),
+        ),
+        (
+            True,
+            (0, 300, 600, 900, 1200),
+            (0, 600, 900, 1200),
+            (0, 200000000000000000002, 300000000000000000004, 400000000000000000008),
+            (
+                0,
+                100000000000000000001,
+                200000000000000000002,
+                300000000000000000004,
+                400000000000000000008,
+            ),
+        ),
+        (
+            True,
+            (0, 300, 600, 900, 1200),
+            (0, 1200),
+            (0, 800000000000000000016),
+            (
+                0,
+                200000000000000000004,
+                400000000000000000008,
+                600000000000000000012,
+                800000000000000000016,
+            ),
+        ),
+    ),
+)
+def test_should_interpolate_data_as_expected(
+    is_super_big_int, ts_indexes, original_ts, original_data, expected
+):
+    interpolated = transformers.interpolate_data(
+        ts_indexes, original_ts, original_data, force_int=is_super_big_int
+    )
+    assert interpolated == expected
+
+
+def test_should_interpolate_date_with_given_timeout():
+    generated = transformers.generate_tf_indexes(1729900800, 1730073600, "5m")
+    is_super_big_int, ts_indexes, original_ts, original_data, expected = (
+        True,
+        generated,
+        (generated[0], generated[50], generated[200], generated[-1]),
+        (0, 300000000000000000004, 400000000000000000008, 800000000000000000016),
+        (0, 300000000000000000004, 400000000000000000008, 800000000000000000016),
+    )
+    assert len(
+        transformers.interpolate_data(
+            ts_indexes, original_ts, original_data, force_int=is_super_big_int
+        )
+    ) == len(generated)
+
+    exper = "transformers.interpolate_data(ts_indexes, original_ts, original_data, force_int=is_super_big_int)"
+    context = locals()
+    context.update(globals())
+    total_cost = timeit.timeit(
+        exper,
+        number=1000,
+        globals=context,
+    )
+    assert total_cost <= 3
