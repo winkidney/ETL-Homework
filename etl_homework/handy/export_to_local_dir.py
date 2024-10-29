2f827c75885df24ebd3f1ef370d5f7aa88e77e26
winkidney <winkidney@gmail.com>
2024-10-30 00:31:51 +0800

    fix: permission workaround for docker

--- etl_homework/handy/export_to_local_dir.py
+++ etl_homework/handy/export_to_local_dir.py
@@ -1,4 +1,6 @@
 import os
+import stat
+import shutil
 
 from etl_homework import exporters
 from etl_homework.dbs import with_db
@@ -8,7 +10,17 @@ from etl_homework.dbs import with_db
 def export_to_local_dir():
     _, csv_report = exporters.CoinMetricsExporter.export()
     new_file = os.path.basename(csv_report)
-    os.rename(csv_report, new_file)
+    shutil.copy(csv_report, new_file)
+    os.chmod(
+        new_file,
+        stat.S_IRUSR
+        | stat.S_IWUSR
+        | stat.S_IRGRP
+        | stat.S_IWGRP
+        | stat.S_IROTH
+        | stat.S_IWOTH,
+    )
+    os.remove(csv_report)
     print("exported to: ", new_file)
 
 
